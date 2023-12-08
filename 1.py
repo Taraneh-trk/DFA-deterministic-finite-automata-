a4d745c9d6152f58b912d03cf1340ac9846025b8
Taraneh <sztu1380@gmail.com>
2023-12-08 18:00:25 +0330

    1_creating-dfa-class-and-menu-class

---
+++ 1.py
@@ -0,0 +1,97 @@
+# all dfas
+dfalist = []
+
+class dfa:
+    def __init__(self,alphabet,state,init_state,final_state,transition):
+        self.alphabet =  alphabet #set of alphabet
+        self.state = state       #sef of state
+        self.init_state = init_state
+        self.final_state = final_state #set of final state
+        self.transition = transition #dictionary of  (cur_state,alphabet) : next_state
+
+    def isempty(self):
+    # true -> lang is empty   &   false -> lang is NOT empty 
+        if len(self.final_state)==0 : 
+            return True
+        
+        for q in self.final_state : 
+            if not self.isunreachable(q) :
+                return False
+        else:
+            return True
+
+    def isfinite(self):
+        pass
+    def isaccept(self):
+        pass
+    def minimize(self):
+        pass
+    def isequal(self):
+        pass
+    def isunreachable(self,q):
+        pass
+
+class   Menu:
+    def __init__(self):
+        while True:
+            print('options : ')
+            print('\t1- enter a dfa')
+            print('\t2- is the machine language empty ? ')
+            print('\t3- is the machine language finite ? ')
+            print('\t4- is x string accepted or not ? ')
+            print('\t5- minimizing dfa')
+            print('\t6- are the two dfa equal ?')
+            print('\t7- exit')
+            choose = int(input('enter your selection : \n'))
+            if choose==1:
+                self.enter()
+            elif choose==2:
+                self.isempty()
+            elif choose==3:
+                self.isfinite()
+            elif choose==4:
+                self.isaccept()
+            elif choose==5:
+                self.minimize()
+            elif choose==6:
+                self.isequal()
+            elif choose==7:
+                exit()
+
+    def enter(self):
+        print('please enter dfa\'s data : ')
+        alphabet = set(input('write each letter of your alphabet and leave a space between each of them : \n').strip(' ').split(' '))
+        state = set(input('write state\'s name of your dfa and leave a space between each of them : \n').strip(' ').split(' '))
+        init_state = input('write the name of initial state : \n')
+        final_state = set(input('write final states name of your dfa and leave a space between each of them : \n').strip(' ').split(' '))
+        if final_state=={''}:
+            final_state.clear()
+        transition = {}
+        for cur_state in state:
+            for alpha_transfer in alphabet:
+                next_state = input(f'enter the state which   {cur_state}   goes with   {alpha_transfer}   : \n')
+                transition[(cur_state,alpha_transfer)] = next_state
+
+        dfalist.append(dfa(alphabet,state,init_state,final_state,transition))
+
+
+    def isempty(self):
+        if dfalist[0].isempty() :
+            print('\tempty\n')
+        else:
+            print('\tNOT empty\n')
+
+    def isfinite(self):
+        pass
+
+    def isaccept(self):
+        pass
+
+    def minimize(self):
+        pass
+
+    def isequal(self):
+        pass
+
+if __name__=='__main__':
+    menu = Menu()
